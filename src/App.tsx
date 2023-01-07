267345c815aec6a5185b27d289d2f582d243e1ee
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-01-07 20:25:53 +0530

    remove about us page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import HomePage from './screens/HomePage';
 import LoginPage from './screens/LoginPage';
-import AboutUsPage from './screens/AboutUsPage';
 import SignUpPage from './screens/SignUpPage'
 
 function App() {
@@ -10,8 +9,6 @@ function App() {
     <HashRouter>
       <Switch>
         <Route exact path="/" component={HomePage}>
-        </Route>
-        <Route exact path="/about" component={AboutUsPage}>
         </Route>
         <Route exact path="/login" component={LoginPage}>
         </Route>
