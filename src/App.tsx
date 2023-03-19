45d5038be1d1b90e0302b5acb78520a571829dda
PasinduTY <pasindu.t.y.s@gmail.com>
2023-03-19 11:36:24 +0530

    dashboard implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React, {useEffect} from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import HomePage from './screens/HomePage';
 import LoginPage from './screens/LoginPage';
 import SignUpPage from './screens/SignUpPage'
 import { AuthProvider } from "@asgardeo/auth-react";
 import { useAuthContext } from "@asgardeo/auth-react"; 
+import Dashboard from "./screens/Dashboard";
 
 
 function App() {
+  
 
   const { state, signIn, signOut } = useAuthContext();
 
@@ -15,6 +16,7 @@ function App() {
     <>
     <HashRouter>
       <Switch>
+        <Route path="/dashboard" element= {<Dashboard/>}/>
         <Route exact path="/" component={state.isAuthenticated ?LoginPage:HomePage}></Route>
         <Route exact path="/signup" component={SignUpPage}>
         </Route>
