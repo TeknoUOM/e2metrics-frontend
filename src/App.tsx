19ed8904550115f47fb2e3f6cd8b7b40ccaec982
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-03-26 17:34:53 +0530

    ui implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import HomePage from './screens/HomePage';
-import LoginPage from './screens/LoginPage';
-import SignUpPage from './screens/SignUpPage'
-import { AuthProvider } from "@asgardeo/auth-react";
-import { useAuthContext } from "@asgardeo/auth-react"; 
-
+import HomePage from "./screens/HomePage";
+import LoginPage from "./screens/LoginPage";
+import SignUpPage from "./screens/SignUpPage";
+import { useAuthContext } from "@asgardeo/auth-react";
+import Payment from "./screens/Payment/Payment";
+import { Payhere, AccountCategory } from "payhere-js-sdk";
+import FeaturePage from "./screens/FeaturePage/FeaturePage";
+import PricePage from "./screens/PricePage/PricePage";
+import PaymentPage from "./screens/Payment/PaymentPage";
+import AddRepositories from "./screens/AddRepositories/Index";
+import PickRepositories from "./screens/PickRepository/Index";
 
 function App() {
+  Payhere.init(
+    "1222632",
+    AccountCategory.SANDBOX,
+    "MTg3MTQwNzgwMjEzNTEyOTQxODkyMTM2Mjk2Njc0ODA5NjEyODgy"
+  );
 
-  const { state, signIn, signOut } = useAuthContext();
+  const { state } = useAuthContext();
 
   return (
-    <>
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={state.isAuthenticated ?LoginPage:HomePage}></Route>
-        <Route exact path="/signup" component={SignUpPage}>
-        </Route>
-      </Switch>
-    </HashRouter>
-    </>
+    <div id="payhere-modal">
+      <HashRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={state.isAuthenticated ? LoginPage : HomePage}
+          ></Route>
+          <Route exact path="/signup" component={SignUpPage}></Route>
+          <Route exact path="/payment" component={Payment}></Route>
+          <Route
+            exact
+            path="/addRepositories/start"
+            component={AddRepositories}
+          ></Route>
+          <Route exact path="/addRepositories/repos/:user/:repo">
+            <PickRepositories />
+          </Route>
+          <Route exact path="/addRepositories/repos">
+            <PickRepositories />
+          </Route>
+          <Route exact path="/feature" component={FeaturePage}></Route>
+          <Route exact path="/price" component={PricePage}></Route>
+          <Route exact path="/checkout" component={PaymentPage}></Route>
+        </Switch>
+      </HashRouter>
+    </div>
   );
 }
 
